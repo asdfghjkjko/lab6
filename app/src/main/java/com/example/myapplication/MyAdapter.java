da28ffaa27e2c8bae8c0fc1eb3d84849430b4fed
asdfghjkjko <asd65432195@gmail.com>
2023-11-10 15:03:17 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import android.content.ClipData;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
